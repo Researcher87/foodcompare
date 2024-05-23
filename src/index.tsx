8bdff341e6932ead08a84f69e1774d4664839c6c
Researcher87 <patrick.lpz87@gmail.com>
2024-05-23 18:55:22 +0200

    Fixes snap Fehler.

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,9 @@ import App from './App';
 const rootElement = document.getElementById("root");
 
 if (rootElement && rootElement.hasChildNodes()) {
-    hydrate(<div><App/></div>, rootElement);
+    hydrate(<App/>, rootElement);
 } else {
     render(
-        <div><App/></div>, rootElement);
+        <App/>, rootElement);
 }
 
