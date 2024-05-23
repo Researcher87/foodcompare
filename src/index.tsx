90b48228fcdb32e0c677cd50e7b32c74021ef547
Researcher87 <patrick.lpz87@gmail.com>
2024-05-23 18:10:03 +0200

    Fixes snap bug.

--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,13 @@ import React from 'react';
 import {hydrate, render} from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const rootElement = document.getElementById("root");
 
-/* if (rootElement && rootElement.hasChildNodes()) {
+if (rootElement && rootElement.hasChildNodes()) {
     hydrate(<div><App/></div>, rootElement);
 } else {
     render(
         <div><App/></div>, rootElement);
-} */
-
-render(<div><App/></div>, rootElement);
-
-
-// reportWebVitals();
-
+}
 
